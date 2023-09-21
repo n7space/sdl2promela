53a389f84e49eab5e5d65aa3c29cb929331fdc2f
Rafał Babski <rbabski@n7space.com>
2023-09-21 18:42:10 +0100

    Fix for special case of 'observe' procedure in observer

--- sdl2promela/translator.py
+++ sdl2promela/translator.py
@@ -2898,8 +2898,8 @@ def translate(
         )
     # Inlines for procedures must be before the transitions
     for procedure in sdl_model.procedures.values():
-        if (procedure.type != sdlmodel.ProcedureType.EXTERNAL) and (
-            is_observer and procedure.name != "observe"
+        if (procedure.type != sdlmodel.ProcedureType.EXTERNAL) and not (
+            procedure.name == "observe" and is_observer
         ):
             if (
                 procedure.type == sdlmodel.ProcedureType.EXPORTED
